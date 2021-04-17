a5cab45dc57f99702e0caf05d15770b51d91d23b
Angela Purcell <kalmpurcell@gmail.com>
2021-04-17 21:10:43 +0000

    cannot commit to staging branch

---
+++ static/js/app.js
@@ -0,0 +1,12 @@
+import React from  'react';
+
+import {Switch, Route} from 'react-router';
+
+function App(){
+    return 
+    <Switch>
+        <Route>
+            <Signin />
+        </Route>
+    </Switch>;
+}
